8dfe0338d1f6b503ca35b1fcb9abe15b08b9ba6c
barinskaya <1388zxc@gmail.com>
2022-10-07 14:35:43 +0300

    Добавьте файлы проекта.

---
+++ MyLabWork6-2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyLabWork6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("a="); //просим пользователя ввести число
+            a = Convert.ToInt32(Console.ReadLine()); //задание переменной целого типа
+            Console.WriteLine("b="); //просим пользователя ввести число
+            b = Convert.ToInt32(Console.ReadLine()); //задание переменной целого типа
+            
+            Console.WriteLine($"{a}+{b}={b}+{a}"); //вывод на экран суммы чисел и суммы чисел в обратном порядке
+        }
+    }
+}
